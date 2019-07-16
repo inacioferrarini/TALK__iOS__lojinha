5d57cae3f6b16a9022c673ed28861193a4c25de5
Inacio Ferrarini <inacio.ferrarini@accenture.com>
2019-07-16 17:57:25 -0300

    Initial version

---
+++ lojinha/lojinha/Init/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  lojinha
+//
+//  Created by Inácio Ferrarini on 16/07/19.
+//  Copyright © 2019 inacio. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var coordinator: MainCoordinator?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let tabBarController = UITabBarController()
+        
+        coordinator = MainCoordinator(tabBarController: tabBarController)
+        
+        coordinator?.start()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
