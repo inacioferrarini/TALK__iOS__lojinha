add3f7bb1a4670587742e7310d41e3e3146c3212
Inacio Ferrarini <inacio.ferrarini@accenture.com>
2019-07-17 12:35:06 -0300

    WIP - Improves project architecture

---
+++ modules/_lojinha_Common/Example/lojinha_Common/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  lojinha_Common
+//
+//  Created by inacio.ferrarini@gmail.com on 07/16/2019.
+//  Copyright (c) 2019 inacio.ferrarini@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
