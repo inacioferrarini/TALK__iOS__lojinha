abeb2993ef4d4a26876d9bdff2d1c17b682e90c2
Inacio Ferrarini <inacio.ferrarini@accenture.com>
2019-07-17 15:17:16 -0300

    Migrated files to submodules

--- modules/Common/Common.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import Common
-
-var str = "Hello, playground"
--- modules/StoreFront/StoreFront.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import StoreFront
-
-var str = "Hello, playground"
