b442036c7ab94fb66abe42ecd51eb1ef9699b196
Inacio Ferrarini <inacio.ferrarini@accenture.com>
2019-08-01 18:35:49 -0300

    Updates common

--- README.md
+++ README.md
@@ -24,3 +24,60 @@ $ git submodule update
 ```bash
 $ git pull --recurse-submodules
 ```
+
+# Adding a new Module
+Create a folder called temp
+Create the module using cookiecutter template
+
+```bash
+$ cookiecutter https://github.com/cookiecutter-swift/FrameworkTemplate
+```
+
+Once the module is created:
+
+* check the created 
+* check the created `Gemfile` file 
+ 
+```ruby
+source 'https://rubygems.org'
+
+gem 'fastlane', '~> 2.120.0'
+gem 'cocoapods', '~> 1.7.4'
+gem 'slather', '~> 2.4.7'
+```
+
+* check the created `.podspec` file
+* remove the created `.swift-version` file
+* execute `bundle exec pod lib lint --sources=INACIO_PODSPECS,master`
+* push it to the remote repository
+
+For modules used by other modules, it is required to push the podspec to the private repository
+```bash
+$ git tag 'v0.0.2'
+$ git push --tags
+$ bundle exec pod repo update INACIO_PODSPECS
+$ bundle exec pod repo push INACIO_PODSPECS Common.podspec
+```
+
+Go to `modules` folder
+
+```bash
+$ git submodule add https://github.com/inacioferrarini/lojinha-login Login
+```
+
+Finally, add the new module to the main App.
+
+Edit `lojinha/Podfile`
+
+Add the new dependency
+
+```ruby
+pod 'Login', :path => '../modules/Login'
+```
+
+Execute Cocoapods
+
+```bash
+$ bundle exec pod install
+```
+
--- lojinha/lojinha/Modules/Main/Coordinators/MainCoordinator.swift
+++ lojinha/lojinha/Modules/Main/Coordinators/MainCoordinator.swift
@@ -1,40 +1,26 @@
-//
-//  MainCoordinator.swift
-//  lojinha
-//
-//  Created by Inácio Ferrarini on 16/07/19.
-//  Copyright © 2019 inacio. All rights reserved.
-//
-
-import UIKit
+import Foundation
 import Common
+import StoreFront
 
-class MainCoordinator: NSObject, Coordinator {
+public class MainCoordinator: NSObject, Coordinator {
     
     var childCoordinators = [Coordinator]()
     var tabBarController: UITabBarController
     
-    init(tabBarController: UITabBarController) {
+    public init(tabBarController: UITabBarController) {
         self.tabBarController = tabBarController
     }
     
-    func start() {
-        // Create UITabBarController itens programatically
+    public func start() {
         print("Initing tab bar")
         loadModules()
-        
-        
-//        if let vc = ViewController.instantiate() {
-//            vc.delegate = self
-//            navigationController.pushViewController(vc, animated: false)
-//        } else {
-//            let coordinatorName = String(describing: self)
-//            logFault(category: "ViewController", message: "Coordinator %{PUBLIC}@ failed to load ViewController %{PUBLIC}@ - instantiate() failed.", coordinatorName, "ViewController")
-//        }
-
     }
     
     internal func loadModules() {
+        self.tabBarController.viewControllers = [
+            StoreFront().viewController,
+            StoreFront().viewController
+        ]
         print("loading modules ...")
     }
     
